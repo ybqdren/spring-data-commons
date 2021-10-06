c72bd6de9e8321f146d3136a159e65a04ce38fc3
Mark Paluch <mpaluch@vmware.com>
2021-10-06 15:13:51 +0200

    Add support for factory method discovery.

--- src/main/java/org/springframework/data/mapping/FactoryMethod.java
+++ src/main/java/org/springframework/data/mapping/FactoryMethod.java
@@ -16,46 +16,41 @@
 package org.springframework.data.mapping;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.springframework.core.annotation.MergedAnnotations;
-import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.util.Assert;
 import org.springframework.util.ReflectionUtils;
 
 /**
- * Value object to encapsulate the constructor to be used when mapping persistent data to objects.
+ * Value object to encapsulate the factory method to be used when mapping persistent data to objects.
  *
- * @author Oliver Gierke
- * @author Jon Brisbin
- * @author Thomas Darimont
- * @author Christoph Strobl
  * @author Mark Paluch
- * @author Myeonghyeon Lee
+ * @since 3.0
  */
-public final class PreferredConstructor<T, P extends PersistentProperty<P>> implements EntityCreator<T, P> {
+public final class FactoryMethod<T, P extends PersistentProperty<P>> implements EntityCreator<T, P> {
 
-	private final Constructor<T> constructor;
+	private final Method factoryMethod;
 	private final List<Parameter<Object, P>> parameters;
 	private final Map<PersistentProperty<?>, Boolean> isPropertyParameterCache = new ConcurrentHashMap<>();
 
 	/**
-	 * Creates a new {@link PreferredConstructor} from the given {@link Constructor} and {@link Parameter}s.
+	 * Creates a new {@link FactoryMethod} from the given {@link Constructor} and {@link Parameter}s.
 	 *
 	 * @param constructor must not be {@literal null}.
 	 * @param parameters must not be {@literal null}.
 	 */
 	@SafeVarargs
-	public PreferredConstructor(Constructor<T> constructor, Parameter<Object, P>... parameters) {
+	public FactoryMethod(Method factoryMethod, Parameter<Object, P>... parameters) {
 
-		Assert.notNull(constructor, "Constructor must not be null!");
+		Assert.notNull(factoryMethod, "Factory method must not be null!");
 		Assert.notNull(parameters, "Parameters must not be null!");
 
-		ReflectionUtils.makeAccessible(constructor);
-		this.constructor = constructor;
+		ReflectionUtils.makeAccessible(factoryMethod);
+		this.factoryMethod = factoryMethod;
 		this.parameters = Arrays.asList(parameters);
 	}
 
@@ -64,8 +59,8 @@ public final class PreferredConstructor<T, P extends PersistentProperty<P>> impl
 	 *
 	 * @return
 	 */
-	public Constructor<T> getConstructor() {
-		return constructor;
+	public Method getFactoryMethod() {
+		return factoryMethod;
 	}
 
 	/**
@@ -77,43 +72,19 @@ public final class PreferredConstructor<T, P extends PersistentProperty<P>> impl
 		return parameters;
 	}
 
-	/**
-	 * Returns whether the constructor has {@link Parameter}s.
-	 *
-	 * @see #isNoArgConstructor()
-	 * @return
-	 */
-	public boolean hasParameters() {
-		return !parameters.isEmpty();
-	}
-
 	@Override
 	public int getParameterCount() {
 		return parameters.size();
 	}
 
-	/**
-	 * Returns whether the constructor does not have any arguments.
-	 *
-	 * @see #hasParameters()
-	 * @return
-	 */
-	public boolean isNoArgConstructor() {
-		return parameters.isEmpty();
-	}
-
-	/**
-	 * Returns whether the constructor was explicitly selected (by {@link PersistenceConstructor}).
-	 *
-	 * @return
-	 */
-	public boolean isExplicitlyAnnotated() {
-		return MergedAnnotations.from(constructor).isPresent(PersistenceConstructor.class);
+	@Override
+	public boolean isConstructorParameter(PersistentProperty<?> property) {
+		return isFactoryMethodParameter(property);
 	}
 
 	/**
 	 * Returns whether the given {@link PersistentProperty} is referenced in a constructor argument of the
-	 * {@link PersistentEntity} backing this {@link PreferredConstructor}.
+	 * {@link PersistentEntity} backing this {@link FactoryMethod}.
 	 * <p>
 	 * Results of this call are cached and reused on the next invocation. Calling this method for a
 	 * {@link PersistentProperty} that was not yet added to its owning {@link PersistentEntity} will capture that state
@@ -122,8 +93,7 @@ public final class PreferredConstructor<T, P extends PersistentProperty<P>> impl
 	 * @param property must not be {@literal null}.
 	 * @return {@literal true} if the {@link PersistentProperty} is used in the constructor.
 	 */
-	@Override
-	public boolean isConstructorParameter(PersistentProperty<?> property) {
+	public boolean isFactoryMethodParameter(PersistentProperty<?> property) {
 
 		Assert.notNull(property, "Property must not be null!");
 
@@ -144,25 +114,6 @@ public final class PreferredConstructor<T, P extends PersistentProperty<P>> impl
 		isPropertyParameterCache.put(property, result);
 
 		return result;
-	}
-
-	/**
-	 * Returns whether the given {@link Parameter} is one referring to an enclosing class. That is in case the class this
-	 * {@link PreferredConstructor} belongs to is a member class actually. If that's the case the compiler creates a first
-	 * constructor argument of the enclosing class type.
-	 *
-	 * @param parameter must not be {@literal null}.
-	 * @return {@literal true} if the {@link PersistentProperty} maps to the enclosing class.
-	 */
-	public boolean isEnclosingClassParameter(Parameter<?, P> parameter) {
-
-		Assert.notNull(parameter, "Parameter must not be null!");
-
-		if (parameters.isEmpty() || !parameter.isEnclosingClassParameter()) {
-			return false;
-		}
-
-		return parameters.get(0).equals(parameter);
 	}
 
 }
